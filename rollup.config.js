15eb5dbf3726ebedf354aff495b8cae3c3af2eb8
Sebastiaan Arendsen <sebastiaan@tetra.nl>
2019-10-17 17:23:08 +0200

    Added unit tests

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import typescript from 'rollup-plugin-typescript2';
 import clear from 'rollup-plugin-clear';
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from "rollup-plugin-commonjs";
 import screeps from 'rollup-plugin-screeps';
 
 let cfg;
 const dest = process.env.DEST;
+
 if (!dest) {
  console.log("No destination specified - code will be compiled but not uploaded");
 } else if ((cfg = require("./screeps.json")[dest]) == null) {
@@ -16,6 +18,7 @@ export default {
   plugins: [
     clear({ targets: ['dist'] }),
     resolve(),
+    commonjs(),
     typescript({tsconfig: './tsconfig.json'}),
     screeps({ config: cfg, dryRun: cfg == null }),
   ],
